8179b849f352aad1e56301242dcdf52a7932bb6c
DavidHidde <dahibo@kpnmail.nl>
2021-12-11 20:26:48 +0100

    Finish importing current funnyrolebot and add docker-compose

--- py_bot/cogs/funny_roles.py
+++ py_bot/cogs/funny_roles.py
@@ -2,118 +2,128 @@ import logging
 import pickle
 
 from os import path
-from nextcord import Colour
-from nextcord.ext import commands
-from nextcord.utils import get
-from discord_ui.cogs import slash_command
+
+import discord
+from discord import Member, Colour, Role
+from discord.ext import commands
+from discord.utils import get
 
 
 class FunnyRolesCog(commands.Cog):
     """Cog that allows for letting users add funny roles through commands"""
+
+    bot: discord.Bot
+    secrets: dict
+    max_roles: int
+    file_name: str
+    user_credit: dict
 
     def __init__(self, bot, secrets, **options):
         self.bot = bot
         self.secrets = secrets
         self.max_roles = options.get('max_roles', 10)
         self.file_name = options.get('file_name', 'creditstore')
+        self.user_credit = self.load_credit()
+        self.logger = logging.getLogger('FunnyRolesCog')
 
-        self.load_credit()
+        self.logger.info('Initialized funny roles cog')
 
-    @slash_command(name="giverole")
-    async def give_role(self, ctx, user, *, role_name: str):
+    @commands.slash_command(name="give_role")
+    async def give_role(self, ctx, user: Member, role_name: str):
         """Give a role to a user"""
         # Input parsing
         if not role_name or len(role_name) == 0 or role_name == ' ':
-            return await ctx.send('Missing argument role name')
+            return await ctx.respond('Missing argument role name')
         role_name = role_name.strip()
 
         # Check if author can give roles
-        author = ctx.message.author
+        author = ctx.author
         if self.check_credit(author.id):
-            return await ctx.send('You have no role credit left')
+            return await ctx.respond('You have no role credit left')
 
         # Check if the role already exists, else create it
         guild = ctx.guild
         role = get(guild.roles, name=role_name)
         if role in user.roles:
-            return await ctx.send(f'{user.display_name} already has that role')
+            return await ctx.respond(f'{user.display_name} already has that role')
         if not role:
             role = await guild.create_role(name=role_name, colour=Colour.random())
 
         # Finally, add the role
         try:
             await user.add_roles(role)
-            credit = self.bot.user_credit.get(author.id, None)
+            credit = self.user_credit.get(author.id, None)
             if credit:
-                self.bot.user_credit[author.id].append((role_name, user.id))
+                self.user_credit[author.id].append((role_name, user.id))
             else:
-                self.bot.user_credit[author.id] = [(role_name, user.id)]
+                self.user_credit[author.id] = [(role_name, user.id)]
         except Exception:
-            return await ctx.send(f"Couldn't give role '{role_name}' to {user.display_name}")
+            return await ctx.respond(f"Couldn't give role '{role_name}' to {user.display_name}")
 
         self.save_credit()
-        return await ctx.send(f"Gave role '{role_name}' to {user.display_name}")
+        return await ctx.respond(f"Gave role '{role_name}' to {user.display_name}")
 
-    @slash_command(name="removerole")
-    async def remove_role(self, ctx, user, *, role_name: str):
+    @commands.slash_command(name="remove_role")
+    async def remove_role(self, ctx, user: Member, role: Role):
         """Remove a role given by a user"""
         # Input parsing
-        if not role_name or len(role_name) == 0 or role_name == ' ':
-            return await ctx.send('Missing argument role name')
-        role_name = role_name.strip()
+        if not role:
+            return await ctx.respond('Missing argument role name')
 
         # Check if the author gave that role to the target
-        author = ctx.message.author
-        user_credit = self.bot.user_credit.get(author.id)
-        if not user_credit or (role_name, user.id) not in user_credit:
-            return await ctx.send(f"You didn't give {user.display_name} that role")
+        author = ctx.author
+        user_credit = self.user_credit.get(author.id)
+        if not user_credit or (role.name, user.id) not in user_credit:
+            return await ctx.respond(f"You didn't give {user.display_name} that role")
 
         # Remove role
-        role = get(ctx.guild.roles, name=role_name)
         try:
             await user.remove_roles(role)
-            user_credit.remove((role_name, user.id))
+            user_credit.remove((role.name, user.id))
             if not role.members:
                 await role.delete()
-            self.bot.user_credit[author.id] = user_credit
+            self.user_credit[author.id] = user_credit
         except Exception:
-            return await ctx.send(f"Couldn't remove role '{role_name}' from {user.display_name}")
+            return await ctx.respond(f"Couldn't remove role '{role.name}' from {user.display_name}")
 
         self.save_credit()
-        return await ctx.send(f"Removed role '{role_name}' from {user.display_name}")
+        return await ctx.respond(f"Removed role '{role.name}' from {user.display_name}")
 
-    @slash_command(name="rolecredit")
+    @commands.slash_command(name="role_credit")
     async def role_credit(self, ctx):
-        """Check how many roles a user can still give away"""
-        author = ctx.message.author
-        role_list = self.bot.user_credit.get(author.id, None)
+        """Check how many roles you can still give away"""
+        if not self.user_credit:
+            return await ctx.respond('No roles have been given away yet')
+
+        author = ctx.author
+        role_list = self.user_credit.get(author.id)
         count = len(role_list) if role_list else 0
         if count == 0:
-            return await ctx.send('You have not given away any role yet')
+            return await ctx.respond('You have not given away any role yet')
 
+        # Display roles in a nice manner
         message = f'You have given away {count} roles:\n\n'
         for role, target in role_list:
             user = await self.bot.fetch_user(target)
             message += f" - '{role}' to {user.display_name}\n"
-        return await ctx.send(message + f'\nYou can still give away {self.ma - count} roles\n')
+        return await ctx.respond(message + f'\nYou can still give away {self.max_roles - count} roles\n')
 
     def save_credit(self):
         """Save user_credit[] in file_name"""
         with open(self.file_name, 'wb') as credit_file:
-            pickle.dump(self.bot.user_credit, credit_file)
-        logging.getLogger('FunnyRolesCog').info('Saved credits in file')
+            pickle.dump(self.user_credit, credit_file)
+        self.logger.debug('Saved credits in file')
 
     def load_credit(self):
         """Load user_credit[] from file_name if it exists"""
         if path.exists(self.file_name):
             with open(self.file_name, 'rb') as credit_file:
-                self.bot.user_credit = pickle.load(credit_file)
-            logging.getLogger('FunnyRolesCog').info('Loaded credits')
+                return pickle.load(credit_file)
         else:
-            logging.getLogger('FunnyRolesCog').info('No credits file found')
-            self.bot.user_credit = {}
+            self.logger.debug('No credits file found')
+            return {}
 
     def check_credit(self, user_id):
         """Check if the user is at max credit"""
-        role_list = self.bot.user_credit.get(user_id, None)
-        return role_list != None and len(role_list) >= self.max_roles
+        role_list = self.user_credit.get(user_id)
+        return role_list is not None and len(role_list) >= self.max_roles
--- py_bot/main.py
+++ py_bot/main.py
@@ -1,30 +1,26 @@
-import nextcord
 import logging
 import traceback
 import json
 
-from nextcord.ext import commands
-from discord_ui import UI, SlashOption
-
+from discord import Bot
 from util import Extension, Cog
 
-logger = logging.getLogger('nextcord-main')
+logger = logging.getLogger('discord-main')
 logging.basicConfig(level=logging.DEBUG)
 
 # Setup the bot classes
-client = commands.Bot(" ")
-ui = UI(client)
+bot = Bot()
 
 # Load cogs and extensions
 secrets = json.load(open('secrets.json'))
 try:
     for extension in secrets['extensions']:
-        client.load_extension(Extension(**extension).get_path())
+        bot.load_extension(Extension(**extension).get_path())
 
     for cog in secrets['cogs']:
         parsed_cog = Cog(**cog)
         cog_class = parsed_cog.get_class()
-        client.add_cog(cog_class(client, secrets, **parsed_cog.get_options()))
+        bot.add_cog(cog_class(bot, secrets, **parsed_cog.get_options()))
 except Exception as err:
     logger.debug(traceback.format_exc())
     logger.error(err)
@@ -32,24 +28,16 @@ except Exception as err:
     exit(0)
 
 
-@client.event
+@bot.event
 async def on_ready():
     logger.info('Bot ready to go')
 
 
-@client.event
+@bot.event
 async def on_error(event, *args, **kwargs):
     """General error handling"""
     logger.error(f'Error caused by {event}')
 
 
-@ui.slash.command("hello_world", guild_ids=[308355983745220619])
-async def command(ctx, cool=True):
-    """This is a simple slash command"""
-    # you can use docstrings for the slash command description too
-    logger.info(f'Yes I can read')
-    await ctx.respond("You said this libary is " + str(cool))
-
-
 # Run the bot
-client.run(secrets['token'])
+bot.run(secrets['token'])
